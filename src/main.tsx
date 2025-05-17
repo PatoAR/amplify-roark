6bed864078495bd831052385d12b592d6a7d634b
PatoAR <48461650+PatoAR@users.noreply.github.com>
2025-05-17 20:13:38 -0300

    fixed import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NewsSocketClient from "./NewsSocketClient.tsx";
+import NewsSocketClient from "./NewsSocketClient";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
