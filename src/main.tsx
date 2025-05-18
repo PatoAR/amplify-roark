4ea2a8c2133e15142444044a980cfad9eaa442f7
PatoAR <48461650+PatoAR@users.noreply.github.com>
2025-05-18 12:30:37 -0300

    refactor NewsSocketClient.tsx function

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NewsSocketClient from "./NewsSocketClient";
+import NewsSocketClient from "./NewsSocketClient.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
