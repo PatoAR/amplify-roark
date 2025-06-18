3a9a385d172d9478a199881c53c97173f6b8043c
PatoAR <pja2004@columbia.edu>
2025-06-18 15:58:49 -0300

    adjust amplify_outputs.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,32 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
 import { BrowserRouter } from 'react-router-dom';
-//import { ResourcesConfig } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx"
-//import config from './aws-exports';
 import outputs from './amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 import "./index.css";
 
 Amplify.configure(outputs);
 
-// Custom Header component for the Authenticator
-const components = {
-  Header() {
-    return (
-      <p></p>  
-    );
-  },
-  Footer() {
-    return (
-    <p></p>
-  )}
-};
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator components={components}>
+    <Authenticator>
         <BrowserRouter>
           <App />
         </BrowserRouter>
