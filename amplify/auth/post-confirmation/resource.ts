1ff545182de27351f9d67509b67246ed8134779d
PatoAR <pja2004@columbia.edu>
2025-08-06 08:24:31 -0300

    added environment variables to functions

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -3,4 +3,8 @@ import { defineFunction } from '@aws-amplify/backend';
 export const postConfirmation = defineFunction({
   name: 'postConfirmation',
   entry: './handler.ts',
+  environment: {
+    APPSYNC_URL: '${data.url}',
+    APPSYNC_API_KEY: '${data.api_key}',
+  },
 }); 
--- amplify/functions/referral-processor/resource.ts
+++ amplify/functions/referral-processor/resource.ts
@@ -3,4 +3,8 @@ import { defineFunction } from '@aws-amplify/backend';
 export const referralProcessor = defineFunction({
   name: 'referral-processor',
   entry: './handler.ts',
+  environment: {
+    APPSYNC_URL: '${data.url}',
+    APPSYNC_API_KEY: '${data.api_key}',
+  },
 }); 
